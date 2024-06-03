7cb6bb953e8fe8ce899c49f329a1be9d2b34dd03
VÍCTOR SILVEIRA RODRIGUES <victor_rodrigues2@estudante.sesisenai.org.br>
2024-06-03 12:37:29 -0300

    poke

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-
 import './App.css'
 import Pokedex from './components/Pokedex'
+import React,{ useState, useEffect } from 'react'
+import style from 'styled-components'
+import {motion} from "framer-motion"
+
+
+
+const Spinner = style(motion.div)`
+width:50px;
+height:50px;
+border: 4px solid black;
+border-top: 4px solid red;
+border-radius: 50%;
+display: inline-block;
+`;
+
+const Container =style.div`display:flex;
+width: 100vw;
+align-items: center;
+justify-content: center;
+height:100vh;
+`
+
 
 function App() {
- 
+ const [carregando, setCarregando] = useState(true);
+ useEffect(()=>{
+  const tempo = setTimeout(()=>{
+    setCarregando(false);
+  }, 5000)
+  return () => clearTimeout(tempo);
+ },[])
 
   return (
     <>
-     <Pokedex/>
+     <Container>
+      {carregando ?(
+     <Spinner animate = {{rotate: 360}}
+     transition={{duration: 1, repeat: Infinity, ease: 'linear'}}
+     >
+
+     </Spinner>
+     ):(
+      <Pokedex/>
+     )} 
+     </Container>
     </>
   )
 }
